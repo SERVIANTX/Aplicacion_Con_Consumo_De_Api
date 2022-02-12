8bbfc9bc02e3ea820cf995eed379cd79380a6001
SERVIANTX <eduardoas0223@gmail.com>
2022-02-12 15:55:59 -0500

    FUNCION POST TERMINADO

--- Assets/js/guardar.js
+++ Assets/js/guardar.js
@@ -3,16 +3,19 @@ function guardar()
 	$.ajax(
 		{
 			method:"POST",
-			url:"http://",
-			data:{codigo:$("#txtCodigo").val(),nombre:$("#txtNombre").val()}
+			url:"http://localhost/AngelaMaria/public/api/empleados",
+			data:{nombre:$("#txtnombre").val(),apellido_paterno:$("#txtapellido_paterno").val(),apellido_materno:$("#txtapellido_materno").val(),numero_documento_identidad:$("#txtnumero_documento_identidad").val(),pais:$("#txtpais").val()}
 		}
 	)
 	.done(function(response){
 
 		$("#message").text("El empleado se registro satisfactoriamente");
-
-		$("#txtCodigo").val("");
-		$("#txtNombre").val("");
+		
+		$("#txtnombre").val("");
+		$("#txtapellido_paterno").val("");
+		$("#txtapellido_materno").val("");
+		$("#txtnumero_documento_identidad").val("");
+		$("#txtpais").val("");
 
 	});	
 }	
