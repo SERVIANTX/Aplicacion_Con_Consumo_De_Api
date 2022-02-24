9212d7f4a54287fe27f9131d3bb484880008abd3
Jose-FD <jf153413@gmail.com>
2022-02-24 17:05:07 -0500

    DIESÑO DEL PANEL ADMINISTRADOR

---
+++ views/home/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        
+        <?php
+            require_once("../html/MainHead.php")
+        ?>
+        <title>Inicio</title>
+
+        <link rel="icon" type="image/png" href="../../assets/img/favicon.png">
+    </head>
+
+    <body>
+
+        <!-- Start Sidemenu Area -->
+        <?php
+            require_once("../html/MainMenu.php")
+        ?>
+        <!-- End Sidemenu Area -->
+
+        <!-- Start Main Content Wrapper Area -->
+        <div class="main-content d-flex flex-column">
+
+            <!-- Top Navbar Area -->
+            <?php
+            require_once("../html/MainNavbar.php")
+            ?>
+            <!-- End Top Navbar Area -->
+            
+            <!-- Breadcrumb Area -->
+            <div class="breadcrumb-area">
+                <h1>Dashboard</h1>
+
+                <ol class="breadcrumb">
+                <li class="item"><a href="../home"><i class='bx bx-home-alt'></i></a></li>
+
+                    <li class="item">Dashboard</li>
+
+                </ol>
+            </div>
+            <!-- End Breadcrumb Area -->
+
+            <div class="flex-grow-1"></div>
+
+            <!-- Start Footer End -->
+            <?php
+            require_once("../html/MainFooter.php")
+            ?>
+            <!-- End Footer End -->
+
+        </div>
+        <!-- End Main Content Wrapper Area -->
+        <?php
+            require_once("../html/MainJS.php")
+        ?>
+    </body>
+</html>
