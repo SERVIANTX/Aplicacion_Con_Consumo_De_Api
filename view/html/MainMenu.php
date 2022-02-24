f6d69df423389a251100d7e0f2ae17045dbf08f7
Jose-FD <jf153413@gmail.com>
2022-02-24 18:25:26 -0500

    ACTUALIZACION DE DISEÑO DEL PANEL ADMINISTRATIVO

--- view/html/MainMenu.php
+++ view/html/MainMenu.php
@@ -1,41 +1,65 @@
-<!-- Vendors Min JS -->
-<script src="../../assets/js/vendors.min.js"></script>
+<div class="sidemenu-area">
+    <div class="sidemenu-header">
+        <a href="dashboard-analytics.html" class="navbar-brand d-flex align-items-center">
+            <img src="..\..\assets\img\small-logo.png" alt="image">
+            <span>Fiva</span>
+        </a>
 
-<!-- ApexCharts JS -->
-<script src="../../assets/js/apexcharts/apexcharts.min.js"></script>
-<script src="../../assets/js/apexcharts/apexcharts-stock-prices.js"></script>
-<script src="../../assets/js/apexcharts/apexcharts-irregular-data-series.js"></script>
-<script src="../../assets/js/apexcharts/apex-custom-line-chart.js"></script>
-<script src="../../assets/js/apexcharts/apex-custom-pie-donut-chart.js"></script>
-<script src="../../assets/js/apexcharts/apex-custom-area-charts.js"></script>
-<script src="../../assets/js/apexcharts/apex-custom-column-chart.js"></script>
-<script src="../../assets/js/apexcharts/apex-custom-bar-charts.js"></script>
-<script src="../../assets/js/apexcharts/apex-custom-mixed-charts.js"></script>
-<script src="../../assets/js/apexcharts/apex-custom-radialbar-charts.js"></script>
-<script src="../../assets/js/apexcharts/apex-custom-radar-chart.js"></script>
+        <div class="burger-menu d-none d-lg-block">
+            <span class="top-bar"></span>
+            <span class="middle-bar"></span>
+            <span class="bottom-bar"></span>
+        </div>
 
-<!-- ChartJS -->
-<script src="../../assets/js/chartjs/chartjs.min.js"></script>
-<div class="chartjs-colors"> <!-- To use template colors with Javascript -->
-    <div class="bg-primary"></div>
-    <div class="bg-primary-light"></div>
-    <div class="bg-secondary"></div>
-    <div class="bg-info"></div>
-    <div class="bg-success"></div>
-    <div class="bg-success-light"></div>
-    <div class="bg-danger"></div>
-    <div class="bg-warning"></div>
-    <div class="bg-purple"></div>
-    <div class="bg-pink"></div>
+        <div class="responsive-burger-menu d-block d-lg-none">
+            <span class="top-bar"></span>
+            <span class="middle-bar"></span>
+            <span class="bottom-bar"></span>
+        </div>
+    </div>
+
+    <div class="sidemenu-body">
+        <ul class="sidemenu-nav metisMenu h-100" id="sidemenu-nav" data-simplebar="">
+
+            <li class="nav-item-title">
+                Menu
+            </li>
+
+            <li class="nav-item">
+                <a href="../home" class="nav-link">
+                    <span class="icon"><i class='bx bx-message'></i></span>
+                    <span class="menu-title">Inicio</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="../mntproducto" class="nav-link">
+                    <span class="icon"><i class='bx bx-badge-check'></i></span>
+                    <span class="menu-title">Mnt Producto</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <span class="icon"><i class='bx bx-calendar'></i></span>
+                    <span class="menu-title">Mnt Producto</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <span class="icon"><i class='bx bx-calendar'></i></span>
+                    <span class="menu-title">Enviar Correo</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <span class="icon"><i class='bx bx-log-out'></i></span>
+                    <span class="menu-title">Cerrar Sesión</span>
+                </a>
+            </li>
+
+        </ul>
+    </div>
 </div>
-
-<!-- jvectormap Min JS -->
-<script src="../../assets/js/jvectormap-1.2.2.min.js"></script>
-<!-- jvectormap World Mil JS -->
-<script src="../../assets/js/jvectormap-world-mill-en.js"></script>
-<!-- Custom JS -->
-<script src="../../assets/js/custom.js"></script>
-
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
-
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
